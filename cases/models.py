475daa2ba8cfcec4ec4af54c54ef22a99c416429
evekemo <kemoeverlyne@gmail.com>
2020-05-04 09:37:30 +0300

    added cases models

--- cases/models.py
+++ cases/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cases(models.Model)
+   date = models.DateTimeField(auto_now_add=True)
+   country = models.CharField (max_length=50)
+   Region = models.CharField(max_length=50)
