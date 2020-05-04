4c30f122e55b32784841df1e004b110b22b63c9e
evekemo <kemoeverlyne@gmail.com>
2020-05-04 10:14:34 +0300

    updated the cases model

--- cases/models.py
+++ cases/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Cases(models.Model):
-   date = models.DateTimeField(auto_now_add=True)
+   date = models.DateField(auto_now_add=False)
    country = models.CharField (max_length=50)
    Region = models.CharField(max_length=50)
