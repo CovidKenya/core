1a55470416ab9ca78a79ac1bcdd6946eb6d97b3c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-05-05 16:35:41 +0300

    Created models for cases app

--- cases/models.py
+++ cases/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Visuals(models.Model):
+    country = models.CharField(max_length=20)
+    province = models.CharField(max_length=20)
+    timeline = models.TextField(max_length=None)
+
+    def __str__(self):
+        return self.country
+        return self.province
+        return self.timeline
