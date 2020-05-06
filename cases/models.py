8d4df6d748c5dbd15ed8c5463796b1a73d5c21d8
paulonteri <onteripaul@gmail.com>
2020-05-07 00:33:11 +0300

    Updated the Cases model

--- cases/models.py
+++ cases/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Cases(models.Model):
-    date = models.DateField(auto_now_add=False)
-    country = models.CharField(max_length=50)
-    Region = models.CharField(max_length=50)
-
-
 class Visual(models.Model):
     """
     specified the country as the pk to allow for easy update/create
@@ -25,3 +19,21 @@ class Visual(models.Model):
 
     def __str__(self):
         return f'{self.country} {self.case}'
+
+
+class KenyanCase(models.Model):
+    county = models.CharField(max_length=255)
+    date = models.DateField()
+    cases = models.IntegerField()
+    death = models.IntegerField()
+    recovery = models.IntegerField()
+    #
+    time_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date', 'county']
+        unique_together = [['date', 'county']]
+
+    def __str__(self):
+        return f'{self.county} {self.date}'
