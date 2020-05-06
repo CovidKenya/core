5b978c47098f917969a5c5b5a053e2a0bec08754
paulonteri <onteripaul@gmail.com>
2020-05-06 23:58:20 +0300

    Added remove_provinces to the get_historical_data

--- cases/services/clean_data.py
+++ cases/services/clean_data.py
@@ -1,12 +1,16 @@
-
-
 def remove_provinces(data, date_range):
+    """
+    REMOVE PROVINCES
+    :param data: The Data received from the API
+    :param date_range: the date range of the data
+    :return: data after removing provinces
+    """
     countries_with_provinces = []
     names_of_countries_with_prov = []
 
     # get countries with provinces
     for country in data[:]:
-        if country['province'] != None:
+        if country['province'] is not None:
             if country['country'] not in names_of_countries_with_prov:
                 names_of_countries_with_prov.append(country['country'])
             countries_with_provinces.append(data.pop(data.index(country)))
@@ -26,7 +30,7 @@ def remove_provinces(data, date_range):
             cs = 0
             dt = 0
             rc = 0
-            # per province
+            # sum data up per province
             for prov in countries:
                 cs += prov["timeline"]["cases"][date]
                 dt += prov["timeline"]["deaths"][date]
@@ -34,6 +38,7 @@ def remove_provinces(data, date_range):
             cases[date] = cs
             recovered[date] = rc
             deaths[date] = dt
+        # return country after removing provinces
         totals = ({'country': country_name, 'province': None, 'timeline': {
             'cases': cases, 'deaths': deaths, 'recovered': recovered}})
         data.append(totals)
