b36c4cac5b848ef6618b924f97e4c3f9cf22dd5b
paulonteri <onteripaul@gmail.com>
2020-05-12 12:05:02 +0300

    Fix autodiscover_tasks() not working

--- core/celery.py
+++ core/celery.py
@@ -8,7 +8,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery('core')
 
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
