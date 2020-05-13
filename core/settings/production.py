558c136fc0f4908f6ee235f44ccb117c1d9bff04
paulonteri <onteripaul@gmail.com>
2020-05-13 13:40:11 +0300

    added postgresql settings #33

--- core/settings/production.py
+++ core/settings/production.py
@@ -8,8 +8,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 'PORT',
     }
 }
 
